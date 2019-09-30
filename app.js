e2717fb4c1eaa63eefbe3db6121b63e8e06b6384
SP_Penguin <abrcdf1023@gmail.com>
2019-09-30 16:41:54 +0800

    setup develop environment

--- app.js
+++ app.js
@@ -3,17 +3,21 @@ import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials';
 import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph';
 import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold';
 import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic';
-import CKEditorInspector from '@ckeditor/ckeditor5-inspector';
+
+console.log(process.env.production)
 
 ClassicEditor
-    .create( document.querySelector( '#editor' ), {
-        plugins: [ Essentials, Paragraph, Bold, Italic ],
-        toolbar: [ 'bold', 'italic' ]
-    } )
-    .then( editor => {
-      CKEditorInspector.attach( editor );
-      console.log( 'Editor was initialized', editor );
-    } )
-    .catch( error => {
-        console.error( error.stack );
-    } );
+  .create(document.querySelector('#editor'), {
+    plugins: [Essentials, Paragraph, Bold, Italic],
+    toolbar: ['bold', 'italic']
+  })
+  .then(async editor => {
+    if (!process.env.production) {
+      const CKEditorInspector = await import('@ckeditor/ckeditor5-inspector').then(el => el.default)
+      CKEditorInspector.attach(editor);
+    }
+    console.log('Editor was initialized', editor);
+  })
+  .catch(error => {
+    console.error(error.stack);
+  });
