45935c4f18a07cebf72a03e88806fe923ccfaed6
SP_Penguin <abrcdf1023@gmail.com>
2019-09-30 16:56:10 +0800

    fixed folder structure

--- app.js
+++ app.js
@@ -3,11 +3,10 @@ import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials';
 import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph';
 import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold';
 import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic';
-import Image from '@ckeditor/ckeditor5-image/src/image';
 
 ClassicEditor
   .create(document.querySelector('#editor'), {
-    plugins: [ Essentials, Paragraph, Bold, Italic, Image ],
+    plugins: [ Essentials, Paragraph, Bold, Italic ],
     toolbar: ['bold', 'italic']
   })
   .then(async editor => {
